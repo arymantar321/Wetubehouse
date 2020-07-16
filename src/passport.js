10c3f2d1269e34a73780135f23a0d212626c8999
arymantar321 <arymantar321@gmail.com>
2020-07-16 16:10:07 +0900

    heroku

--- src/passport.js
+++ src/passport.js
@@ -4,33 +4,33 @@ import FacebookStrategy from "passport-facebook";
 import User from "./models/User";
 import {
   githubLoginCallback,
-  facebookLoginCallback
+  facebookLoginCallback,
 } from "./controllers/userController";
 import routes from "./routes";
 
 passport.use(User.createStrategy());
 
 passport.use(
-    new GithubStrategy(
-        {
-          clientID: process.env.GH_ID,
-          clientSecret: process.env.GH_SECRET,
-          callbackURL: `http://localhost:4000${routes.githubCallback}`
-        },
-        githubLoginCallback
-      )
-  );  
+  new GithubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SECRET,
+      callbackURL: process.env.PRODUCTION
+        ? `https://gentle-mesa-36337.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`,
+    },
+    githubLoginCallback
+  )
+);
 
 passport.use(
   new FacebookStrategy(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `https://d220d37c526a.ngrok.io${
-        routes.facebookCallback
-      }`,
+      callbackURL: `https://d220d37c526a.ngrok.io${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
-      scope: ["public_profile", "email"]
+      scope: ["public_profile", "email"],
     },
     facebookLoginCallback
   )
