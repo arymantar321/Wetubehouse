a75b869c7e9189e1165c009f3026fa864ca540e7
arymantar321 <arymantar321@gmail.com>
2020-07-16 17:14:33 +0900

    Update db.js
    back

--- src/db.js
+++ src/db.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config(); // .env 파일안에 있는 정보 불러올 수 있음.
 
-mongoose.connect(
-  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
