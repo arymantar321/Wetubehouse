f12f587a0485988dda5a1954fd84253a206b5962
arymantar321 <arymantar321@gmail.com>
2020-07-23 17:10:09 +0900

    Prestart

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -158,7 +158,7 @@ export const postAddComment = async (req, res) => {
     });
     video.comments.push(newComment.id);
     video.save();
-    videoDetail(req, res);
+    videoDetail();
   } catch (error) {
     res.status(400);
   } finally {
